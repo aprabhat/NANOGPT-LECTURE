9c24b692edef5b5f176701d3c817d03fff1ba13d
Prabhat Agarwal <prabhat.agarwal0605@gmail.com>
2024-03-02 23:01:09 +0530

    batch layer

--- train.py
+++ train.py
@@ -34,12 +34,23 @@ n = int(0.9 * len(data))
 train_data = data[:n]  # first 90%
 val_data = data[n:]  # rest 10%
 
+# example
+# x = train_data[:block_size]
+# y = train_data[1:block_size + 1]
+# for t in range(block_size):
+#     context = x[:t + 1]
+#     target = y[t]
+#     print(f"when input is {context} the target is : {target}")
+
+torch.manual_seed(1337)
+batch_size = 4
 block_size = 8
 
-# example
-x = train_data[:block_size]
-y = train_data[1:block_size + 1]
-for t in range(block_size):
-    context = x[:t + 1]
-    target = y[t]
-    print(f"when input is {context} the target is : {target}")
+
+def get_batch(split):
+    # generate a small batch of data of input x and target y
+    data1 = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data1) - block_size, (batch_size,))
+    x = torch.stack([data1[i:i + block_size] for i in ix])
+    y = torch.stack([data1[i + 1, i + block_size + 1] for i in ix])
+    return x, y
